a3840a43989a8e8ee0f39f46229c0f2f28a53c41
vipin.ku <vipin.ku@MMMDVVIPINKU.local>
2019-08-19 12:06:47 +0530

    Database connection established

---
+++ src/main/java/DatabaseService/DBConnect.java
@@ -0,0 +1,54 @@
+package DatabaseService;
+
+import shared.Article;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBConnect {
+    private Connection connnection;
+    private Statement statment;
+    private ResultSet resultSet;
+    public DBConnect(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/newsAggregatorDatabase","root","vipin1407");
+            statment = connnection.createStatement();
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void insertArticle(Article article){
+        try{
+            java.text.SimpleDateFormat simpleDateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String exactDate = simpleDateFormat.format(article.getPubDate());
+            String query = "insert into articles values(" +article.getId() +",\""+article.getTitle()+"\","+article.getCategoryId()+",\""+
+                    article.getUrl()+"\",\""+exactDate+"\",\""+article.getRss()+"\",\""+article.getContent()+"\");";
+            int count = statment.executeUpdate(query);
+            System.out.println("insert row "+count);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public int getCategoryId(String category){
+        int ret = 0;
+        try {
+            String query = "select id from category where name = \""+category+"\";";
+            resultSet = statment.executeQuery(query);
+            while (resultSet.next()){
+                ret = Integer.parseInt(resultSet.getString("id"));
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return ret;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,29 @@
+import DatabaseService.DBConnect;
+import de.l3s.boilerpipe.BoilerpipeProcessingException;
 import de.l3s.boilerpipe.extractors.ArticleExtractor;
+import shared.Article;
+
+import java.net.URL;
+import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) {
+    public static String extractFromUrl(URL url) throws BoilerpipeProcessingException {
+        String output = ArticleExtractor.getInstance().getText(url);
+        return output;
+    }
 
+    public static String extractFromHtml(String html) throws BoilerpipeProcessingException {
+        String output = ArticleExtractor.getInstance().getText(html);
+        return output;
+    }
+    public static void main(String[] args) {
+        DBConnect dbConnect = new DBConnect();
+        try {
+            Article article = new Article(1,"demotitle",dbConnect.getCategoryId("sports"),"demoURL",new Date(),"demoContent","demoRss");
+            dbConnect.insertArticle(article);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
