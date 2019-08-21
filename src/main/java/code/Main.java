7f1070fe3968ccff2161ffb0bb4abd9877c950e0
dipendrasings <dipendrabana@gmail.com>
2019-08-21 16:46:48 +0530

    Data cleaner added

--- src/main/java/code/Main.java
+++ src/main/java/code/Main.java
@@ -1,11 +1,13 @@
 package code;
 
+import code.clusteringComponent.DataCleaner;
 import code.contentComponent.PollingService;
 import code.contentComponent.RssController;
 import code.databaseService.DBConnect;
 import code.models.Article;
 import code.models.ArticleBuilder;
 import code.models.CategoryType;
+import org.apache.log4j.PropertyConfigurator;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -13,6 +15,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+//        System.out.println(DataCleaner.clean("Hello ' th'e of i sas sfdfe ! %$ @, ,,  paying playing player played saying sayer says paid done doer goer goes }} {{ , 303  /["));
+//        PropertyConfigurator.configure("src/main/resources/log4j.properties");
         PollingService.getInstance().poll();
 
 //        List<Article> list = new ArrayList<>();
