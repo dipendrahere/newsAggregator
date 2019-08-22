4446dafd7b08fb210022286ad358b98f8dc996cf
vipin1998 <vipindhangar1407@gmail.com>
2019-08-22 15:45:09 +0530

    Hierarchical clusterer implemented

--- src/main/java/code/Main.java
+++ src/main/java/code/Main.java
@@ -1,55 +1,53 @@
 package code;
 
 import code.clusteringComponent.DataCleaner;
+import code.clusteringComponent.HierarchicalCluster;
+import code.clusteringComponent.PairComparator;
 import code.contentComponent.PollingService;
 import code.contentComponent.RssController;
 import code.databaseService.DBConnect;
 import code.models.Article;
 import code.models.ArticleBuilder;
 import code.models.CategoryType;
+import code.models.SportsTfIdfHelper;
 import code.utility.GlobalFunctions;
 import javafx.util.Pair;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) {
-        //        System.out.println(DataCleaner.clean("Hello ' th'e of i sas sfdfe ! %$ @, ,,  paying playing player played saying sayer says paid done doer goer goes }} {{ , 303  /["));
-//        PropertyConfigurator.configure("src/main/resources/log4j.properties");
-//        PollingService.getInstance().poll();
 
-        /* Test for tf-idf function
-        List<Article> list = new ArrayList<>();
-        Article a = new ArticleBuilder("url4")
-                .setCategoryType(CategoryType.SPORTS)
-                .setContent("This is random shit")
-                .setRssLink("Rss")
-                .setTitle("demo")
-                .setPublishedDate(new Date())
-                .build();
-        Article b = new ArticleBuilder("url5")
-                .setCategoryType(CategoryType.SPORTS)
-                .setContent("This 'is' Bull' shit")
-                .setRssLink("Rss")
-                .setTitle("demo")
-                .setPublishedDate(new Date())
-                .build();
-        Article c = new ArticleBuilder("url3")
-                .setCategoryType(CategoryType.SPORTS)
-                .setContent("This is a great\" article")
-                .setRssLink("Rss")
-                .setTitle("demo")
-                .setPublishedDate(new Date())
-                .build();
-        list.add(a);
-        list.add(b);
-        list.add(c);
+        //PollingService.getInstance().poll();
+//        Article a = new ArticleBuilder("url4")
+//                .setCategoryType(CategoryType.SPORTS)
+//                .setContent("i am vipin kumar")
+//                .setRssLink("Rss")
+//                .setTitle("demo")
+//                .setPublishedDate(new Date())
+//                .build();
+//
+//        Article b = new ArticleBuilder("url3")
+//                .setCategoryType(CategoryType.SPORTS)
+//                .setContent("i am vipin kumar")
+//                .setRssLink("Rss")
+//                .setTitle("demo")
+//                .setPublishedDate(new Date())
+//                .build();
+//        try {
+//            System.out.println(GlobalFunctions.cosineSimilarity(b,a));
+//        }
+//        catch (Exception e){
+//            e.printStackTrace();
+//        }
 
-         */
+        List<Article> list = DBConnect.getInstance().fetchArticles(CategoryType.WORLD);
+        System.out.println(list.size());
+        HierarchicalCluster hierarchicalCluster = new HierarchicalCluster(list);
+        hierarchicalCluster.performClustering();
+        hierarchicalCluster.printLargestCluster();
+
 
     }
 }
