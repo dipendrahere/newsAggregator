972b14a6add36d5015214e1a57ba972aa8da634f
dipendrasings <dipendrabana@gmail.com>
2019-08-29 13:50:10 +0530

    ranking parameters implementation

--- src/main/java/code/models/ClusterInfo.java
+++ src/main/java/code/models/ClusterInfo.java
@@ -1,4 +1,61 @@
 package code.models;
 
+import java.util.Date;
+
 public class ClusterInfo {
+    private int clusterId;
+    private double coverageScore;
+    private Date recency;
+    private int totalPoints;
+    private Date averageDate;
+    private Double diameter;
+
+    public Double getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(Double diameter) {
+        this.diameter = diameter;
+    }
+
+    public int getClusterId() {
+        return clusterId;
+    }
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+    public void setTotalPoints(int totalPoints) {
+        this.totalPoints = totalPoints;
+    }
+
+    public Date getAverageDate() {
+        return averageDate;
+    }
+
+    public void setAverageDate(Date averageDate) {
+        this.averageDate = averageDate;
+    }
+
+    public void setClusterId(int clusterId) {
+        this.clusterId = clusterId;
+    }
+
+    public double getCoverageScore() {
+        return coverageScore;
+    }
+
+    public void setCoverageScore(double coverageScore) {
+        this.coverageScore = coverageScore;
+    }
+
+    public Date getRecency() {
+        return recency;
+    }
+
+    public void setRecency(Date recency) {
+        this.recency = recency;
+    }
+
 }
