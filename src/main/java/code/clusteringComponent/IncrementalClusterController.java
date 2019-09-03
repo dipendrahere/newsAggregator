efddbbd322e75bfc1a20e431b0b575949172fd2f
vipin1998 <vipindhangar1407@gmail.com>
2019-09-03 14:51:19 +0530

    Log removed

--- src/main/java/code/clusteringComponent/IncrementalClusterController.java
+++ src/main/java/code/clusteringComponent/IncrementalClusterController.java
@@ -18,22 +18,14 @@ public class IncrementalClusterController {
     }
     public void run(){
 
-        Log.debug("incremental Start for " + categoryType);
         HashMap<Article,Integer> hashMap = DBConnect.getInstance().articleClusterRelationship(categoryType);
-        Log.debug("debug 1 " + categoryType);
         IncrementalClusterer<Article> hierarchicalClusterer = new IncrementalClusterer<Article>(eps);
         HashMap<String,Integer> updatedClusterId = hierarchicalClusterer.clusterIncrementally(hashMap);
-        Log.debug("debug 2 " + categoryType);
         List<ClusterInfo> existingInfo = DBConnect.getInstance().getClusterInfo();
-        Log.debug("debug 3 " + categoryType);
         List<ClusterInfo> info = new ClusterInfoHelper().incrementDiameters(updatedClusterId, hashMap, existingInfo);
-        Log.debug("debug 4 " + categoryType);
         HashMap<String,Integer> clusterRank = new ClusterInfoHelper().incrementalRanking(updatedClusterId,hashMap);
-        Log.debug("debug 5 " + categoryType);
         DBConnect.getInstance().updateClusterRank(clusterRank);
-        Log.debug("debug 6 " + categoryType);
         DBConnect.getInstance().updateClusterInfo(info);
-        Log.debug("debug 7 " + categoryType);
         DBConnect.getInstance().updateClusterIDs(updatedClusterId);
 
         Log.debug("INCREMENTAL DONE for " + categoryType);
