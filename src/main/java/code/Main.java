659a02c0b8e18a72ea74cfed352e3e87747a5fc6
dipendrasings <dipendrabana@gmail.com>
2019-09-03 13:33:25 +0530

    Start all services

--- src/main/java/code/Main.java
+++ src/main/java/code/Main.java
@@ -3,16 +3,12 @@ package code;
 import code.clusteringComponent.BatchClusterService;
 import code.clusteringComponent.IncrementalClusterService;
 import code.contentComponent.PollingService;
-import code.databaseService.DBConnect;
-import code.models.Cluster;
-import code.models.ClusterInfo;
 
-import java.util.*;
 
 public class Main {
     public static void main(String[] args)  {
         BatchClusterService.getInstance().start();
-    //    IncrementalClusterService.getInstance().start();
-    //    PollingService.getInstance().poll();
+        IncrementalClusterService.getInstance().start();
+        PollingService.getInstance().poll();
     }
 }
